af288f39a59d53a30b81f1822681cbba9d81e556
fgrothaus <felix.grothaus94@gmail.com>
2024-09-06 00:19:17 +0200

    React Anwendung erstellt

---
+++ react_demo/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Component1 } from './components/Component1';
+
+
+
+function App() {
+  return (
+    
+    <React.Fragment>
+      <Component1 />
+    </React.Fragment>
+
+
+  );
+}
+
+export default App;
